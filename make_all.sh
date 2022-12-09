e3265f9f1715e9010baa131e9dd11862a0574247
lorashen <lorashen@126.com>
2022-12-09 16:10:00 +0800

    add make

---
+++ make_all.sh
@@ -0,0 +1,8 @@
+cd /workspace/FasterTransformer/
+mkdir build
+cd build
+cmake -DSM=80 -DCMAKE_BUILD_TYPE=Release -DBUILD_PYT=ON -DBUILD_MULTI_GPU=ON -DCMAKE_PREFIX_PATH=/opt/conda/lib/python3.8/site-packages/pybind11/share/cmake/pybind11 ..
+make -j12
+cd ..
+./build/bin/codegeex_gemm 1 1 32 64 64  16348 50048 1 1 # 10B
+# ./build/bin/gpt_gemm 1 1 32 16 64  4096 50048 0 1 # large
