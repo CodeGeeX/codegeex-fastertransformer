677a7318a04b67b421e4b99ea812a9ae8b49a11f
lorashen <lorashen@126.com>
2023-03-24 22:15:13 +0800

    fix same name function bug

--- src/fastertransformer/kernels/codegeex_top_decoder_masked_multihead_attention.cu
+++ src/fastertransformer/kernels/codegeex_top_decoder_masked_multihead_attention.cu
@@ -702,7 +702,7 @@ template<
     // The number of threads in a threadblock.
     int THREADS_PER_BLOCK,
     bool DO_CROSS_ATTENTION>
-__global__ void masked_multihead_attention_kernel(Multihead_attention_params<T, DO_CROSS_ATTENTION> params)
+__global__ void top_masked_multihead_attention_kernel(Multihead_attention_params<T, DO_CROSS_ATTENTION> params)
 {
 
     // Make sure the hidden dimension per head is a multiple of the number of threads per key.
@@ -1255,10 +1255,10 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T, 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, THDS_PER_KEY, THDS_PER_VALUE, THDS_PER_BLOCK, DO_CROSS_ATTENTION, stream)    \
+#define TOP_MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, THDS_PER_KEY, THDS_PER_VALUE, THDS_PER_BLOCK, DO_CROSS_ATTENTION, stream)    \
     size_t smem_sz = mmha::smem_size_in_bytes<T, DO_CROSS_ATTENTION>(params, THDS_PER_VALUE, THDS_PER_BLOCK);          \
     dim3 grid(params.num_heads, params.batch_size);                                                                    \
-    mmha::masked_multihead_attention_kernel<T,                                                                         \
+    mmha::top_masked_multihead_attention_kernel<T,                                                                         \
                                             Dh,                                                                        \
                                             Dh_MAX,                                                                    \
                                             THDS_PER_KEY,                                                              \
@@ -1270,52 +1270,52 @@ __global__ void masked_multihead_attention_kernel(Multihead_attention_params<T, 
 
 // !!! Specialize the launcher for Cross attention
 template<typename T, int Dh, int Dh_MAX, typename KERNEL_PARAMS_TYPE>
-void mmha_launch_kernel(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& stream)
+void top_mmha_launch_kernel(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& stream)
 {
     constexpr int THREADS_PER_VALUE = Dh_MAX * sizeof(T) / 16;
     constexpr bool DO_CROSS_ATTENTION = std::is_same<KERNEL_PARAMS_TYPE, Cross_multihead_attention_params<T>>::value;
     int tlength = (DO_CROSS_ATTENTION) ? params.seq_length : params.timestep;
     // printf("tlength, CROSS_ATTENTION = %d, %d\n", tlength, DO_CROSS_ATTENTION);
     if (tlength < 32) {
-        MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 4, THREADS_PER_VALUE, 64, DO_CROSS_ATTENTION, stream);
+        TOP_MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 4, THREADS_PER_VALUE, 64, DO_CROSS_ATTENTION, stream);
     }
     else if (tlength < 2048) {
-        MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 2, THREADS_PER_VALUE, 128, DO_CROSS_ATTENTION, stream);
+        TOP_MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 2, THREADS_PER_VALUE, 128, DO_CROSS_ATTENTION, stream);
     }
     else {
-        MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 1, THREADS_PER_VALUE, 256, DO_CROSS_ATTENTION, stream);
+        TOP_MMHA_LAUNCH_KERNEL(T, Dh, Dh_MAX, 1, THREADS_PER_VALUE, 256, DO_CROSS_ATTENTION, stream);
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 template<typename T, typename KERNEL_PARAMS_TYPE>
-void multihead_attention_(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& stream)
+void top_multihead_attention_(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& stream)
 {
     switch (params.hidden_size_per_head) {
         case 32:
-            mmha_launch_kernel<T, 32, 32, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 32, 32, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 64:
-            mmha_launch_kernel<T, 64, 64, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 64, 64, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 96:
-            mmha_launch_kernel<T, 96, 128, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 96, 128, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 128:
-            mmha_launch_kernel<T, 128, 128, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 128, 128, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 160:
-            mmha_launch_kernel<T, 160, 256, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 160, 256, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 192:
-            mmha_launch_kernel<T, 192, 256, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 192, 256, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 224:
-            mmha_launch_kernel<T, 224, 256, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 224, 256, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         case 256:
-            mmha_launch_kernel<T, 256, 256, KERNEL_PARAMS_TYPE>(params, stream);
+            top_mmha_launch_kernel<T, 256, 256, KERNEL_PARAMS_TYPE>(params, stream);
             break;
         default:
             assert(false);
@@ -1326,14 +1326,14 @@ void multihead_attention_(const KERNEL_PARAMS_TYPE& params, const cudaStream_t& 
 
 void top_masked_multihead_attention(const Masked_multihead_attention_params<float>& params, const cudaStream_t& stream)
 {
-    multihead_attention_<float, Masked_multihead_attention_params<float>>(params, stream);
+    top_multihead_attention_<float, Masked_multihead_attention_params<float>>(params, stream);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void top_masked_multihead_attention(const Masked_multihead_attention_params<uint16_t>& params, const cudaStream_t& stream)
 {
-    multihead_attention_<uint16_t, Masked_multihead_attention_params<uint16_t>>(params, stream);
+    top_multihead_attention_<uint16_t, Masked_multihead_attention_params<uint16_t>>(params, stream);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1342,23 +1342,23 @@ void top_masked_multihead_attention(const Masked_multihead_attention_params<uint
 void top_masked_multihead_attention(const Masked_multihead_attention_params<__nv_bfloat16>& params,
                                 const cudaStream_t& stream)
 {
-    multihead_attention_<__nv_bfloat16, Masked_multihead_attention_params<__nv_bfloat16>>(params, stream);
+    top_multihead_attention_<__nv_bfloat16, Masked_multihead_attention_params<__nv_bfloat16>>(params, stream);
 }
 #endif
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void top_cross_multihead_attention(const Cross_multihead_attention_params<float>& params, const cudaStream_t& stream)
 {
-    multihead_attention_<float, Cross_multihead_attention_params<float>>(params, stream);
+    top_multihead_attention_<float, Cross_multihead_attention_params<float>>(params, stream);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void top_cross_multihead_attention(const Cross_multihead_attention_params<uint16_t>& params, const cudaStream_t& stream)
 {
-    multihead_attention_<uint16_t, Cross_multihead_attention_params<uint16_t>>(params, stream);
+    top_multihead_attention_<uint16_t, Cross_multihead_attention_params<uint16_t>>(params, stream);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#undef MMHA_LAUNCH_KERNEL
+#undef TOP_MMHA_LAUNCH_KERNEL
