227ecd42fa6efab52b6eafd82da7def22ff29e6f
shenlei <lorashen@126.com>
2023-06-01 15:14:55 +0800

    modify post file

--- post.py
+++ post.py
@@ -9,15 +9,18 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36",
 }
 
-sentence = "# language: python\n# write a quick sort function\ndef"
+sentence = ["# language: python\n# write a merge sort function\ndef",
+        "# language: python\n# write a merge sort function\ndef",
+        "# language: python\n# write a merge sort function\ndef",
+]
 
 results=[]
 for i in range(1):
     data = json.dumps({'ability': 'seo_article_creation', 
                        'context': sentence, 
-                       'temperature':1.0 ,
-                       'top_k': 1,
-                       'top_p': 0.0,
+                       'temperature':0.2 ,
+                       'top_k': 0,
+                       'top_p': 0.9,
                        'max_seq_len': 256,
                        'len_penalty': 1.0,
                        'repetition_penalty': 1.0,
@@ -29,7 +32,7 @@ for i in range(1):
     r = requests.post(url, data, headers=headers)
     time2=time.time()
     print("time used",time2-time1)
-    print(r.json()['generated'])
+    print(r.json())
     rdict=json.loads(r.text)
-    result={"sentence":sentence,"result":rdict['generated']}
-    results.append(result)
+    #result={"sentence":sentence,"result":rdict['generated']}
+    #results.append(result)
